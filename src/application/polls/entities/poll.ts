de1a83bd54abf082d8e0600d1265979ea89029da
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-02-08 13:21:03 -0300

    refactor: remove unused property

--- src/application/polls/entities/poll.ts
+++ src/application/polls/entities/poll.ts
@@ -1,9 +1,8 @@
-import { Poll, PollOption } from '@prisma/client';
+import { Poll } from '@prisma/client';
 
 export class PollEntity implements Poll {
   id: string;
   title: string;
   created_at: Date;
   updated_at: Date;
-  options: PollOption[];
 }
