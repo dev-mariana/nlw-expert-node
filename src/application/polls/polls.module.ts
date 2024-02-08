a887031402fa8d3e07bf640b74e7c4ff7e22a615
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-02-07 21:15:26 -0300

    feat: create poll at service and inject database module

--- src/application/polls/polls.module.ts
+++ src/application/polls/polls.module.ts
@@ -1,9 +1,11 @@
+import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { PollsController } from './controllers/polls.controller';
 import { PollsRepository } from './repositories/polls.repository';
 import { PollsService } from './services/polls.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PollsController],
   providers: [PollsService, PollsRepository],
 })
--- src/application/polls/services/polls.service.ts
+++ src/application/polls/services/polls.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePollDTO } from '../dto/create-poll.dto';
+import { PollEntity } from '../entities/poll';
+import { PollsRepository } from '../repositories/polls.repository';
 
 @Injectable()
-export class PollsService {}
+export class PollsService {
+  constructor(private readonly pollsRepository: PollsRepository) {}
+
+  async create(CreatePollDTO: CreatePollDTO): Promise<PollEntity> {
+    return await this.pollsRepository.create(CreatePollDTO);
+  }
+}
