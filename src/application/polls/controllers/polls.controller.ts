e869cdec605d838e2bf465f68343da2ae7c3c6c4
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-02-07 21:15:37 -0300

    feat: create poll at controller

--- src/application/polls/controllers/polls.controller.ts
+++ src/application/polls/controllers/polls.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { ZodValidationPipe } from '@shared/pipes/zod-validation.pipe';
+import { CreatePollDTO, createPollSchema } from '../dto/create-poll.dto';
+import { PollEntity } from '../entities/poll';
 import { PollsService } from '../services/polls.service';
 
 @Controller('polls')
 export class PollsController {
   constructor(private readonly pollsService: PollsService) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createPollSchema))
+  async create(@Body() CreatePollDTO: CreatePollDTO): Promise<PollEntity> {
+    return await this.pollsService.create(CreatePollDTO);
+  }
 }
