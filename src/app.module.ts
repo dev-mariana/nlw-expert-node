d87ac554ea12b5b8053c1f03dfe36b9efde0e4fa
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-02-24 12:14:49 -0300

    feat: add redis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { RedisModule } from '@nestjs-modules/ioredis';
 import { Module } from '@nestjs/common';
 import { PollsModule } from './application/polls/polls.module';
 import { DatabaseModule } from './infra/database/database.module';
@@ -6,9 +7,10 @@ import { DatabaseModule } from './infra/database/database.module';
   imports: [
     PollsModule,
     DatabaseModule,
-    // FastifyCookie({
-    //   secret: process.env.SECRET_KEY,
-    // }),
+    RedisModule.forRoot({
+      type: 'single',
+      url: 'localhost:6379',
+    }),
   ],
 })
 export class AppModule {}
