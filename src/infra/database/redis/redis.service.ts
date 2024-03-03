c3698aeb5d4ae8451bab5a8c25d72b92ba86c4ad
Mariana Bastos <maribastossrodrigues@gmail.com>
2024-03-03 15:39:00 -0300

    feat: add getRank at redis service

--- src/infra/database/redis/redis.service.ts
+++ src/infra/database/redis/redis.service.ts
@@ -12,4 +12,17 @@ export class RedisService {
   ): Promise<string> {
     return await this.redisClient.zincrby(sortedSetName, incrementBy, member);
   }
+
+  async getRank(
+    sortedSetName: string,
+    startPosition: number,
+    endPosition: number,
+  ): Promise<string[]> {
+    return await this.redisClient.zrange(
+      sortedSetName,
+      startPosition,
+      endPosition,
+      'WITHSCORES',
+    );
+  }
 }
